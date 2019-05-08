6bfaaad18cd1e2df91949671b5435b5efbc3539b
souvikdgreat <souvikc40@gmail.com>
2019-05-08 11:56:11 +0530

    added datatype

---
+++ datatypes/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+//integers
+//unsigned integer
+let z: u32=4;
+
+//signed 64 bit integer
+let i=15;
+
+let f=1.55;
+
+//declaring boolean
+
+let b:bool=true;
+
+println!("the value of f is {} and b is {}",f,b);
+
+    println!("the value of z and i is {} and {}",z,i);
+}

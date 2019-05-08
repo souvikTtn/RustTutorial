ab4cb77ff3b2cdcf2b971bcd8795f3de8b259328
souvikdgreat <souvikc40@gmail.com>
2019-05-08 11:40:30 +0530

    added some rust programs

---
+++ constant/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+	const X:f32=1.54;
+    println!("the value of x is {}",X);
+}
---
+++ variable/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+	let mut y=15;
+    println!("value of y is {}",y);
+	y=16;
+	println!("value of y is {}",y);
+}
